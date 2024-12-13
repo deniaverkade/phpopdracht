cf6353857b24f39bfae9559f0ebee957787b79e8
deniaverkade <9024406@student.zadkine.nl>
2024-12-13 13:25:34 +0100

    Update navbar.php

---
+++ navbar.php
@@ -0,0 +1,7 @@
+<div id="navigatiebar">
+<nav>
+    <a href="index.php"><ul>Hoofdpagina</ul></a>
+    <a href="about.php"><ul>Over</ul></a>
+    <a href="contact.php"><ul>Contact</ul></a>
+</nav>
+</div>
