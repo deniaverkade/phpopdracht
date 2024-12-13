dd8ad0f4c11e90aaf843b1958ad1960fb5300a06
deniaverkade <9024406@student.zadkine.nl>
2024-12-13 13:12:52 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Opdracht 2, 3 en 4</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <form>
+        <label for="fname">Voornaam:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+
+        <label for="lname">Achternaam:</label><br>
+        <input type="text" id="lname" name="lname"><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="msg">Bericht:</label><br>
+        <input type="tekst" id="msg" name="msg"><br>
+
+        <input type="submit" id="submit" name="submit">
+        </form>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
